bf24647098f5c819af26c0d4fdfbb36136736608
Administrator <Administrator@smart20114>
2019-08-28 12:17:02 +0900

    지역을 클릭하면 그 지역에 해당되는 시들이 나오고 특정 시를 클릭하면 그 지역의 날씨가 나오고 날씨를 클릭하면 시간과 데이터가 나오게 맵으로 분류시켜 놓았다. api를 가지고 와서 활용하는 법

--- apiTest/src/main/java/com/example/apiTest/controller/ApiController.java
+++ apiTest/src/main/java/com/example/apiTest/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.example.apiTest.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +10,8 @@ import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,7 +43,36 @@ public class ApiController {
 					.data("stnId", stnId)
 					.get();
 			
-			System.out.println(doc.html());
+			Elements locations = doc.select("location");
+			
+			for(int i =0; i< locations.size(); i++) {
+				Map<String,Object> locationMap = new HashMap<String,Object>();
+				//객체 하나하나 접근할수 있도록
+				Element location = locations.get(i);
+				Elements datas = location.select("data");			
+				String city = location.select("city").text();
+				
+				locationMap.put("city", city);
+				
+				List<Map<String,Object>> dataList = new ArrayList<Map<String,Object>>();
+				for(int d=0; d<datas.size(); d++) {
+					Map<String,Object> dataMap = new HashMap<String,Object>();
+					Element data =datas.get(d);
+					String time = data.select("tmEf").text();
+					String content = data.select("wf").text();
+					
+					dataMap.put("time", time);
+					dataMap.put("content", content);
+					
+					dataList.add(dataMap);
+				}
+			
+				locationMap.put("dataList", dataList);
+				
+				apiList.add(locationMap);
+			}
+			
+			System.out.println(locations.html());
 			
 		}catch(IOException e) {
 			e.printStackTrace();
